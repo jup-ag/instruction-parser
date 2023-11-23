b2de7c18ecc53aee166b9c81fa36c17c12c2ff75
Arrowana <Arrowana@users.noreply.github.com>
2023-11-24 10:05:38 +1100

    feat: Relax types and type for multiple upstreams

---
+++ src/types.ts
@@ -0,0 +1,34 @@
+import { IdlEvents, IdlTypes } from "@coral-xyz/anchor";
+import { Jupiter } from "./idl/jupiter";
+import { ParsedInstruction, PublicKey } from "@solana/web3.js";
+
+export type SwapEvent = IdlEvents<Jupiter>["SwapEvent"];
+export type FeeEvent = IdlEvents<Jupiter>["FeeEvent"];
+type RoutePlanStep = IdlTypes<Jupiter>["RoutePlanStep"];
+export type RoutePlan = RoutePlanStep[];
+
+export interface PartialInstruction {
+  programId: PublicKey;
+  data: string /** Expecting base58 */;
+  // accountKeys are not necessary
+}
+
+// Subset of @solana/web3.js ParsedTransactionWithMeta to allow flexible upstream data
+export interface TransactionWithMeta {
+  meta: {
+    logMessages?: string[] | null;
+    innerInstructions?:
+      | {
+          index: number;
+          instructions: (ParsedInstruction | PartialInstruction)[];
+        }[]
+      | null;
+  } | null;
+  transaction: {
+    signatures: string[];
+    message: {
+      accountKeys: { pubkey: PublicKey }[];
+      instructions: (ParsedInstruction | PartialInstruction)[];
+    };
+  };
+}
