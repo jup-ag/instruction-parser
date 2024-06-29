091787147f699f2c6aa61dfa188ec38b6aacac87
jokrhub <loganalex007@gmail.com>
2024-06-29 10:58:27 +0530

    add token account positions and swap directions

--- src/constants.ts
+++ src/constants.ts
@@ -58,40 +58,220 @@ export const STACK_HEIGHT = {
 };
 
 export const SWAP_IN_OUT_ACCOUNTS_POSITION = {
-  "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8": {
-    in: 15,
-    out: 16,
+  AMM55ShdkoGRB5jVYPjWziwk8m5MpwyDgsMWHaMSQWH6: {
+    in: 7,
+    out: 8,
   },
-  opnb2LAfJYbRMAHHvqjCwQxanZn7ReEHp1k81EohpZb: {
+  CURVGoZn8zycx6FXwwevgBTB2gVvdbGTEpvMJDbgs2t4: {
+    in: 7,
+    out: 8,
+  },
+  D3BBjqUdCYuP18fNvvMbPAZ8DpcRi4io2EsYHQawJDag: {
+    in: 6,
+    out: 9,
+  },
+  BSwp6bEBihVLdqJRKGgzjcGLHkcTuzmSo1TQkHepzH8p: {
     in: 9,
     out: 10,
   },
-  whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc: {
-    in: 3,
-    out: 5,
+  C1onEW2kPetmHmwe74YC1ESx3LnFEpVau6g2pg4fHycr: {
+    in: 0,
+    out: 0,
   },
-  MERLuDFBMmsHnsBPZw2sDQZHvXFMwp8EdjudcU2HKky: {
-    in: 8,
-    out: 9,
-  },
-  SSwpkEEcbUqx4vtoEByFjSkhKdCT862DNVb52nZg1UZ: {
-    in: 3,
-    out: 6,
-  },
-  H8W3ctz92svYg6mkn1UtGfu2aQr2fnUFHM1RhScEtQDt: {
-    in: 3,
+  CLMM9tUoggJu2wagPkkqs9eFG4BWhVBZWkP1qv3Sp7tR: {
+    in: 4,
     out: 5,
   },
   CTMAxxk34HjKWxQ3QLZK1HpaLXmBveao3ESePXbiyfzh: {
     in: 4,
     out: 7,
   },
+  cysPXAjehMpVKUapzbMCCnpFxUFFryEWEaLgnb9NrR8: {
+    in: 0,
+    out: 0,
+  },
+  GNExJhNUhc9LN2DauuQAUJnXoy6DJ6zey3t9kT9A2PF3: {
+    in: 2,
+    out: 3,
+  },
+  dp2waEWSBy5yKmq65ergoU3G6qRLmqa6K7We4rZSKph: {
+    in: 9,
+    out: 10,
+  },
+  "7WduLbRfYhTJktjLw5FDEyrqoEv61aTTCuGAetgLjzN5": {
+    in: 8,
+    out: 9,
+  },
+  GFXsSL5sSaDfNFQUYsHekbWBW1TsFdjDYzACh62tEHxn: {
+    in: 5,
+    out: 6,
+  },
+  Gswppe6ERWKpUTXvRPfXdzHhiCyJvLadVvXGfdpBqcE1: {
+    in: 6,
+    out: 7,
+  },
+  treaf4wWBBty3fHdyBpo35Mz84M8k3heKXmjmi9vFt5: {
+    in: 5,
+    out: 6,
+  },
+  HyaB3W9q6XdA5xwpU4XnSZV94htfmbmqJXZcEbRaJutt: {
+    in: 3,
+    out: 4,
+  },
+  EewxydAPCCVuNEyrVN68PuSYdQ7wKn27V9Gjeoi8dy3S: {
+    in: 3,
+    out: 4,
+  },
+  "2wT8Yq49kHgDzXuPxZSaeLaH1qbmGXtEyPy64bL7aD3c": {
+    in: 3,
+    out: 4,
+  },
+  "9tKE7Mbmj4mxDjWatikzGAtkoWosiiZX9y6J4Hfm2R8H": {
+    in: 6,
+    out: 7,
+  },
+  MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD: {
+    in: 0,
+    out: 0,
+  },
+  MERLuDFBMmsHnsBPZw2sDQZHvXFMwp8EdjudcU2HKky: {
+    in: -2,
+    out: -1,
+  },
+  Eo7WjKq67rjJQSZxS6z3YkapzY3eMj6Xy8X5EQVn5UaB: {
+    in: 1,
+    out: 2,
+  },
+  LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo: {
+    in: 4,
+    out: 5,
+  },
+  srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX: {
+    in: 0,
+    out: 0,
+  },
+  opnb2LAfJYbRMAHHvqjCwQxanZn7ReEHp1k81EohpZb: {
+    in: 9,
+    out: 10,
+  },
+  PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu: {
+    in: 1,
+    out: 2,
+  },
+  PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY: {
+    in: 4,
+    out: 5,
+  },
+  "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8": {
+    in: 14,
+    out: 15,
+  },
+  CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK: {
+    in: 3,
+    out: 4,
+  },
+  CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C: {
+    in: 4,
+    out: 5,
+  },
+  SSwpkEEcbUqx4vtoEByFjSkhKdCT862DNVb52nZg1UZ: {
+    in: 3,
+    out: 6,
+  },
+  DecZY86MU5Gj7kppfUCEmd4LbXXuyZH1yHaP2NTqdiZB: {
+    in: 0,
+    out: 0,
+  },
+  "5ocnV1qiCgaQR8Jb8xWnVbApfaygJ8tNoZfgPwsgx9kx": {
+    in: 3,
+    out: 4,
+  },
+  SCHAtsf8mbjyjiv4LkhLKutTf6JnZAbdJKFkXQNMFHZ: {
+    in: 3,
+    out: 6,
+  },
+  "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin": {
+    in: 0,
+    out: 0,
+  },
+  stkitrT1Uoy18Dk1fTrgPw8W6MVzoCfYoAFT4MLsmhq: {
+    in: 1,
+    out: 2,
+  },
+  SSwpMgqNDsyV7mAgN9ady4bDVu5ySjmmXejXvy2vLt1: {
+    in: 3,
+    out: 6,
+  },
+  "2KehYt3KsEQR53jYcxjbQp2d2kCp4AkuQW68atufRwSr": {
+    in: 4,
+    out: 6,
+  },
+  DjVE6JNiYqPL2QXyCUUh8rNjHrbz9hXHNYt99MQ59qw1: {
+    in: 3,
+    out: 6,
+  },
+  "9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP": {
+    in: 3,
+    out: 6,
+  },
+  PSwapMdSai8tjrEXcxFeQth87xC4rRsa4VA5mhGhXkP: {
+    in: 3,
+    out: 6,
+  },
+  SSwapUtytfBdBn1b9NUGG6foMVPtcWgpRU32HToDUZr: {
+    in: 3,
+    out: 6,
+  },
+  Dooar9JkhdZ7J3LHN3A7YCuoGRUggXhQaG4kijfLGU2j: {
+    in: 3,
+    out: 6,
+  },
+  SwaPpA9LAaLfeLi3a68M4DjnLqgtticKg6CnyNwgAC8: {
+    in: 3,
+    out: 6,
+  },
+  FLUXubRmkEi2q6K3Y9kBPg9248ggaZVsoSFhtJHSrm1X: {
+    in: 3,
+    out: 6,
+  },
+  DSwpgjMvXhtGn6BsbqmacdBZyfLj6jSWf3HJpdJtmg6N: {
+    in: 3,
+    out: 6,
+  },
+  whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc: {
+    in: 3,
+    out: 5,
+  },
+  H8W3ctz92svYg6mkn1UtGfu2aQr2fnUFHM1RhScEtQDt: {
+    in: 3,
+    out: 5,
+  },
 };
 
 export const SWAP_DIRECTION_ARGS = {
   NO_ARG: [
-    "MERLuDFBMmsHnsBPZw2sDQZHvXFMwp8EdjudcU2HKky",
     "SSwpkEEcbUqx4vtoEByFjSkhKdCT862DNVb52nZg1UZ",
+    "DecZY86MU5Gj7kppfUCEmd4LbXXuyZH1yHaP2NTqdiZB",
+    "SwaPpA9LAaLfeLi3a68M4DjnLqgtticKg6CnyNwgAC8",
+    "SCHAtsf8mbjyjiv4LkhLKutTf6JnZAbdJKFkXQNMFHZ",
+    "SSwpMgqNDsyV7mAgN9ady4bDVu5ySjmmXejXvy2vLt1",
+    "CTMAxxk34HjKWxQ3QLZK1HpaLXmBveao3ESePXbiyfzh",
+    "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8",
+    "EewxydAPCCVuNEyrVN68PuSYdQ7wKn27V9Gjeoi8dy3S",
+    "MERLuDFBMmsHnsBPZw2sDQZHvXFMwp8EdjudcU2HKky",
+    "cysPXAjehMpVKUapzbMCCnpFxUFFryEWEaLgnb9NrR8",
+    "MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD",
+    "Eo7WjKq67rjJQSZxS6z3YkapzY3eMj6Xy8X5EQVn5UaB",
+    "7WduLbRfYhTJktjLw5FDEyrqoEv61aTTCuGAetgLjzN5",
+    "D3BBjqUdCYuP18fNvvMbPAZ8DpcRi4io2EsYHQawJDag",
+    "2wT8Yq49kHgDzXuPxZSaeLaH1qbmGXtEyPy64bL7aD3c",
+    "CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK",
+    "treaf4wWBBty3fHdyBpo35Mz84M8k3heKXmjmi9vFt5",
+    "stkitrT1Uoy18Dk1fTrgPw8W6MVzoCfYoAFT4MLsmhq",
+    "GFXsSL5sSaDfNFQUYsHekbWBW1TsFdjDYzACh62tEHxn",
+    "PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu",
+    "LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo",
+    "CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C"
   ],
   SIDE: [
     "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin",
